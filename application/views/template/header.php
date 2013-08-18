02c75b9b83a9f3ac7de07065262f21332b5e4460
Randall Perkins <rppandkaw@yahoo.com>
2013-08-18 19:45:01 -0400

    Updated  header.php example

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -2,7 +2,6 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<?php if(isset($header_title)) { echo "<title>" . $header_title . "</title>"; }else{ echo "<title>YOUR_SITE_NAME</title>"; }  ?>
 		<?php if(isset($header_description)) { echo '<meta name="description" content="'.$header_description.'">'; }else{ echo '<meta name="description" content="a free and open community for gamers and game lovers">'; }  ?>
 		<?php if(isset($header_keywords)) { echo "" . $header_keywords . "<meta name='keywords' content='".$header_keywords."'>"; } ?>
 		<?php if(isset($header_scripts)) { echo $header_scripts; }  ?>
